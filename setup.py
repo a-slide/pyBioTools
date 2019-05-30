0d3e78c4c12d24b97a51c5a87585e2df0b14ee70
a-slide <aleg@ebi.ac.uk>
2019-05-30 17:12:01 +0100

    Move stuff between init and setup

--- setup.py
+++ setup.py
@@ -2,5 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-from pyBioTools import setup_dict
-setup(**setup_dict)
+import pyBioTools as pbt
+
+# Collect info in a dictionnary for setup.py
+setup(
+    name =  pbt.__name__,
+    version = pbt.__version__,
+    description = pbt.__description__,
+    url = "https://github.com/a-slide/pyBioTools",
+    author = 'Adrien Leger',
+    author_email = 'aleg {at} ebi.ac.uk',
+    license = 'GPLv3',
+    python_requires ='>=3.5',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3'],
+    install_requires = [
+        'tqdm==4.32.1',
+        'numpy==1.16.4',
+        'pysam==0.15.2',
+        'pandas==0.24.2'],
+    packages = [pbt.__name__],
+    entry_points = {'console_scripts': ['pyBioTools=pyBioTools.__main__:main']})
