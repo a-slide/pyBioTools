360205bde95417c6affbc94a69dbf22e6047db27
a-slide <aleg@ebi.ac.uk>
2019-10-11 17:53:14 +0100

    Improve autodoc from docstr

--- pyBioTools/__main__.py
+++ pyBioTools/__main__.py
@@ -37,45 +37,61 @@ def main ():
     sp_a_subparsers = sp_a.add_subparsers (description="Alignment implements the following subcommands", dest="alignment_subcommands")
     sp_a_subparsers.required = True
 
-    # alignment_index_reads subparser
-    f = Alignment.index_reads
-    sp_a_ir = sp_a_subparsers.add_parser("index_reads", description=doc_func(f))
+    # Alignment Reads_index subparser
+    f = Alignment.Reads_index
+    sp_a_ir = sp_a_subparsers.add_parser("Reads_index", description=doc_func(f))
     sp_a_ir.set_defaults(func=f)
-    sp_a_ir.add_argument("-b", "--bam_fn", **arg_opt(f, "bam_fn"))
-    sp_a_ir.add_argument("-m", "--min_mapq", **arg_opt(f, "min_mapq"))
-    sp_a_ir.add_argument("-u", "--keep_unmapped", **arg_opt(f, "keep_unmapped"))
-    sp_a_ir.add_argument("-s", "--keep_secondary", **arg_opt(f, "keep_secondary"))
-    sp_a_ir.add_argument("-p", "--keep_supplementary", **arg_opt(f, "keep_supplementary"))
+    arg_from_docstr(sp_a_ir, f, "input_fn", "i")
+    arg_from_docstr(sp_a_ir, f, "skip_unmapped", "u")
+    arg_from_docstr(sp_a_ir, f, "skip_secondary", "s")
+    arg_from_docstr(sp_a_ir, f, "skip_supplementary", "p")
 
-    # alignment_downsample_reads
-    f = Alignment.sample_reads
-    sp_a_sr = sp_a_subparsers.add_parser("sample_reads", description=doc_func(f))
+    # Alignment Reads_sample
+    f = Alignment.Reads_sample
+    sp_a_sr = sp_a_subparsers.add_parser("Reads_sample", description=doc_func(f))
     sp_a_sr.set_defaults(func=f)
-    sp_a_sr.add_argument("-b", "--bam_fn", **arg_opt(f, "bam_fn"))
-    sp_a_sr.add_argument("-f", "--out_folder", **arg_opt(f, "out_folder"))
-    sp_a_sr.add_argument("-p", "--out_prefix", **arg_opt(f, "out_prefix"))
-    sp_a_sr.add_argument("-r", "--n_reads", **arg_opt(f, "n_reads"))
-    sp_a_sr.add_argument("-s", "--n_samples", **arg_opt(f, "n_samples"))
-    sp_a_sr.add_argument("--rand_seed", **arg_opt(f, "rand_seed"))
+    arg_from_docstr(sp_a_sr, f, "input_fn", "i")
+    arg_from_docstr(sp_a_sr, f, "output_folder" , "o")
+    arg_from_docstr(sp_a_sr, f, "output_prefix" , "p")
+    arg_from_docstr(sp_a_sr, f, "n_reads" , "r")
+    arg_from_docstr(sp_a_sr, f, "n_samples" , "s")
+    arg_from_docstr(sp_a_sr, f, "rand_seed")
+
+    # Alignment Filter
+    f = Alignment.Filter
+    sp_a_fr = sp_a_subparsers.add_parser("Filter", description=doc_func(f))
+    sp_a_fr.set_defaults(func=f)
+    arg_from_docstr(sp_a_fr, f, "input_fn", "i")
+    arg_from_docstr(sp_a_fr, f, "output_fn", "o")
+    arg_from_docstr(sp_a_fr, f, "skip_unmapped", "u")
+    arg_from_docstr(sp_a_fr, f, "skip_secondary", "s")
+    arg_from_docstr(sp_a_fr, f, "skip_supplementary", "p")
+    arg_from_docstr(sp_a_fr, f, "orientation", "t")
+    arg_from_docstr(sp_a_fr, f, "min_read_len", "r")
+    arg_from_docstr(sp_a_fr, f, "min_align_len", "a")
+    arg_from_docstr(sp_a_fr, f, "min_mapq", "m")
+    arg_from_docstr(sp_a_fr, f, "min_freq_identity", "f")
+    arg_from_docstr(sp_a_fr, f, "select_ref")
+    arg_from_docstr(sp_a_fr, f, "exclude_ref")
 
     #~~~~~Fastq suparser~~~~~#
     sp_fq = subparsers.add_parser("Fastq", description="Fastq format related functions")
     sp_fq_subparsers = sp_fq.add_subparsers (description="Fastq implements the following subcommands", dest="fastq_subcommands")
     sp_fq_subparsers.required = True
 
-    # Fastq filter_reads subparser
-    f = Fastq.filter_reads
-    sp_fq_fr = sp_fq_subparsers.add_parser("filter_reads", description=doc_func(f))
+    # Fastq Filter subparser
+    f = Fastq.Filter
+    sp_fq_fr = sp_fq_subparsers.add_parser("Filter", description=doc_func(f))
     sp_fq_fr.set_defaults(func=f)
-    sp_fq_fr.add_argument("-s", "--src_fn", **arg_opt(f, "src_fn"))
-    sp_fq_fr.add_argument("-d", "--dest_fn", **arg_opt(f, "dest_fn"))
-    sp_fq_fr.add_argument("-l", "--min_len", **arg_opt(f, "min_len"))
-    sp_fq_fr.add_argument("-u", "--min_qual", **arg_opt(f, "min_qual"))
-    sp_fq_fr.add_argument("-r", "--remove_duplicates", **arg_opt(f, "remove_duplicates"))
-    sp_fq_fr.add_argument("-o", "--qual_offset", **arg_opt(f, "qual_offset"))
+    arg_from_docstr(sp_fq_fr, f, "input_fn" , "i")
+    arg_from_docstr(sp_fq_fr, f, "output_fn" , "o")
+    arg_from_docstr(sp_fq_fr, f, "min_len" , "l")
+    arg_from_docstr(sp_fq_fr, f, "min_qual" , "u")
+    arg_from_docstr(sp_fq_fr, f, "remove_duplicates" , "r")
+    arg_from_docstr(sp_fq_fr, f, "qual_offset" , "f")
 
     # Add common group parsers
-    for sp in [sp_a_ir, sp_a_sr, sp_fq_fr]:
+    for sp in [sp_a_ir, sp_a_sr, sp_a_fr, sp_fq_fr]:
         sp_verbosity = sp.add_mutually_exclusive_group()
         sp_verbosity.add_argument("-v", "--verbose", action="store_true", default=False, help="Increase verbosity (default: %(default)s)")
         sp_verbosity.add_argument("-q", "--quiet", action="store_true", default=False, help="Reduce verbosity (default: %(default)s)")
