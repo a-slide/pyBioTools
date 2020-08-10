c5a8a42fd59ed068d3ebbafd23aeae863c6842cf
a-slide <aleg@ebi.ac.uk>
2020-08-10 16:03:11 +0100

    Bump up version num

--- pyBioTools/__init__.py
+++ pyBioTools/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # Define self package variable
-__version__ = '0.2.0.8'
+__version__ = '0.2.0.9'
 __description__="pyBioTools is a collection of tools to manipulate biological sequences"
 __all__ = ["Alignment", "Fastq", "Fasta"]
