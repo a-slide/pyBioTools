4ca082d9ae54f04e0e530f8c49aebeca403773ea
a-slide <aleg@ebi.ac.uk>
2021-01-22 12:00:58 +0000

    Improve sequencing summary merge

--- pyBioTools/Seqsum.py
+++ pyBioTools/Seqsum.py
@@ -4,6 +4,7 @@
 from collections import *
 import gzip
 import csv
+import os
 
 # Third party library imports
 from tqdm import tqdm
@@ -40,7 +41,8 @@ def Merge (
     logger.info("Parsing reads")
     c = Counter()
     main_header = None
-
+    read_id_list = []
+    duplicated_ids=0
     try:
         # Define output mode
         open_fun_w, open_mode_w = (gzip.open, "wt") if is_gziped(output_fn) else (open, "w")
@@ -60,6 +62,7 @@ def Merge (
                         header[i]="filename"
 
                     if main_header is None:
+                        read_id_index = header.index("read_id")
                         main_header = header
                         fp_out.write("\t".join(header)+"\n")
 
@@ -70,13 +73,35 @@ def Merge (
                         continue
 
                     # Rest of the lines
-                    fp_out.write(fp_in.read())
+                    for line in tqdm(fp_in, desc="Line processed ", unit=" lines", disable=not progress):
+                        read_id = line.split("\t")[read_id_index]
+                        if read_id in read_id_list:
+                            duplicated_ids+=1
+                        else:
+                            read_id_list.append(read_id)
+                            fp_out.write(line)
+                            c["Valid lines"]+=1
+
                     c["Valid files"]+=1
                     logger.debug("End of file {}".format(fn_in))
 
     except (StopIteration, KeyboardInterrupt):
         pass
 
+    except:
+        logger.error("An error occured. Atempting to delete corrupted output file")
+        try:
+            os.remove(output_fn)
+            logger.error("Output file removed")
+        except:
+            pass
+            logger.error("Could not remove output file")
+        raise
+
+
     # Print read count summary
+    if duplicated_ids:
+        logger.error("{} duplicated read ids in input files".format(duplicated_ids))
+
     logger.info("Read counts summary")
     log_dict(c, logger.info)
